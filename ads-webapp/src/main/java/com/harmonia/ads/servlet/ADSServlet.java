a9bb9a9a5d1b239ecdc071c20c4210ee4194f1e2
Jeffrey Anway <janway@harmonia.com>
2015-06-23 13:58:12 -0400

    Test REST Method.
    * Added a REST endpoint that queries the default OpenFDA /drug/event, which returns a single event.
    * The response is returned to our client as-is.

--- ads-webapp/src/main/java/com/harmonia/ads/servlet/ADSServlet.java
+++ ads-webapp/src/main/java/com/harmonia/ads/servlet/ADSServlet.java
@@ -1,5 +1,7 @@
 package com.harmonia.ads.servlet;
 
+import java.net.URI;
+
 import javax.enterprise.context.RequestScoped;
 import javax.servlet.http.HttpServletRequest;
 import javax.transaction.Transactional;
@@ -8,6 +10,13 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 /**
  * REST endpoint for ADS-related operations
@@ -25,10 +34,31 @@ public class ADSServlet {
 	@Context
 	private HttpServletRequest httpRequest;
 
+	private CloseableHttpClient httpClient = HttpClients.createDefault();
+
+	private final String apiBase = "https://api.fda.gov";
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/test/")
 	public String test() {
 		return "Test";
+	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("event")
+	public String getOneAdverseDrugEvent() {
+		String ret = "";
+		try {
+			URI uri = new URIBuilder().setScheme("https")
+					.setHost("api.fda.gov").setPath("/drug/event.json").build();
+			HttpGet httpget = new HttpGet(uri);
+			CloseableHttpResponse response = httpClient.execute(httpget);
+			ret = EntityUtils.toString(response.getEntity());
+		} catch (Exception e) {
+			// TODO
+		}
+		return ret;
 	}
 }
