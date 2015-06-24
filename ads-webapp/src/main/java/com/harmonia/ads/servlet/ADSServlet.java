1a926d7a4c43f37eec82dd1c2159ff30b66c476e
Jeffrey Anway <janway@harmonia.com>
2015-06-23 20:52:29 -0400

    Partial events endpoint.
    * Implemented the REST endpoint for adverse drug events using patient information.
    * Comments indicate how the drug portion of the query will look, but it's yet to be implemented.

--- ads-webapp/src/main/java/com/harmonia/ads/servlet/ADSServlet.java
+++ ads-webapp/src/main/java/com/harmonia/ads/servlet/ADSServlet.java
@@ -10,6 +10,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.UriInfo;
 
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -60,5 +62,143 @@ public class ADSServlet {
 			// TODO
 		}
 		return ret;
+	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("events")
+	public String getAdverseDrugEvents(@Context UriInfo info) {
+		MultivaluedMap<String, String> params = info.getQueryParameters();
+
+		System.out.println("Retrieved query parameters from UriInfo.");
+
+		String ageParameter = createAgeParameter(params.getFirst("ageStart"),
+				params.getFirst("ageEnd"));
+		System.out.println("age string: " + ageParameter);
+		String dateParameter = createDateParameter(
+				params.getFirst("dateStart"), params.getFirst("dateEnd"));
+		System.out.println("date string: " + dateParameter);
+		String genderParameter = createGenderParameter(params
+				.getFirst("gender"));
+		System.out.println("gender string: " + genderParameter);
+		String weightParameter = createWeightParameter(
+				params.getFirst("weightStart"), params.getFirst("weightEnd"));
+		System.out.println("weight string: " + weightParameter);
+
+		// TODO: need to figure out how to handle the drug parameters
+		// Need to receive a list of tuples, one tuple per drug containing a
+		// subset of
+		// indication, brand name, generic name, manufacturer name, active
+		// ingredients
+		// then convert to the api format, ex
+		// (indication:tuple_1_indication+AND+generic:tuple_1_generic)+AND+(indication:tuple_2_indication)
+
+		String and = "+AND+";
+		StringBuilder sb = new StringBuilder();
+		sb.append(ageParameter);
+		sb.append(and);
+		sb.append(dateParameter);
+		sb.append(and);
+		sb.append(genderParameter);
+		sb.append(and);
+		sb.append(weightParameter);
+
+		String search = sb.toString();
+		System.out.println(search);
+		while (search.contains(and + and)) {
+			search = search.replace(and + and, and);
+		}
+		if (search.startsWith(and)) {
+			search = search.substring(and.length());
+		}
+		if (search.endsWith(and)) {
+			search = search.substring(0, search.length() - and.length());
+		}
+		search.replace(" ", "+");
+
+		System.out.println(search);
+
+		String result = "";
+		try {
+			HttpGet httpget = new HttpGet(
+					"https://api.fda.gov/drug/event.json?search=" + search);
+			CloseableHttpResponse response = httpClient.execute(httpget);
+			result = EntityUtils.toString(response.getEntity());
+		} catch (Exception e) {
+			// TODO
+		}
+
+		return result;
+	}
+
+	private String createAgeParameter(String ageStart, String ageEnd) {
+		if (ageStart != null && ageEnd == null) {
+			ageEnd = ageStart;
+		}
+		if (ageStart == null && ageEnd != null) {
+			ageStart = ageEnd;
+		}
+		String ageConditionString = "";
+		if (ageStart != null) {
+			ageConditionString = "patient.patientonsetage:[" + ageStart
+					+ "+TO+" + ageEnd + "]+AND+patient.patientonsetageunit:801";
+		}
+		return ageConditionString;
+	}
+
+	private String createDateParameter(String dateStart, String dateEnd) {
+		if (dateStart != null && dateEnd == null) {
+			dateEnd = dateStart;
+		}
+		if (dateStart == null && dateEnd != null) {
+			dateStart = dateEnd;
+		}
+		String dateConditionString = "";
+		if (dateStart != null) {
+			dateConditionString = "receivedate:[" + dateStart + "+TO+"
+					+ dateEnd + "]";
+		}
+		return dateConditionString;
+	}
+
+	private String createGenderParameter(String gender) {
+		String genderValue = null;
+		if (gender != null) {
+			if (gender.equalsIgnoreCase("male")) {
+				genderValue = "1";
+			}
+			if (gender.equalsIgnoreCase("female")) {
+				genderValue = "2";
+			}
+			if (gender.equalsIgnoreCase("unknown")) {
+				genderValue = "0";
+			}
+		}
+		String genderConditionString = "";
+		if (genderValue != null) {
+			genderConditionString = "patient.patientsex:" + genderValue;
+		}
+		return genderConditionString;
+	}
+
+	private String createWeightParameter(String weightStart, String weightEnd) {
+		if (weightStart != null)
+			weightStart = "" + Integer.parseInt(weightStart) * 0.453592; // lbs
+																			// to
+																			// kg
+		if (weightEnd != null)
+			weightEnd = "" + Integer.parseInt(weightEnd) * 0.453592;
+		if (weightStart != null && weightEnd == null) {
+			weightEnd = weightStart;
+		}
+		if (weightStart == null && weightEnd != null) {
+			weightStart = weightEnd;
+		}
+		String weightConditionString = "";
+		if (weightStart != null) {
+			weightConditionString = "patient.patientweight:[" + weightStart
+					+ "+TO+" + "weightEnd" + "]";
+		}
+		return weightConditionString;
 	}
 }
